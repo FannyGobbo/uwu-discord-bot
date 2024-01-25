fa9f3b9195b2c109acdb71d2d29afd4b7f03e8a4
FannyGobbo <fanny.gobbo00@gmail.com>
2024-01-25 09:51:38 +0100

    upfixes and categories update

--- uwubot.py
+++ uwubot.py
@@ -13,100 +13,128 @@ intents.messages = True
 
 bot = commands.Bot(command_prefix='!', intents=intents)
 
+
+###########################################################################################################################################
+
+ 
+class Couscous(commands.Cog, name="Couscous"):
+    def __init__(self, bot):
+        self.bot = bot
+    
+    @commands.command(name="count", help="Laissez Fanny faire siouplé")
+    async def count(self,
+                    ctx, 
+                    dd: int = commands.parameter(description="Jour"), 
+                    MM: int = commands.parameter(description="Mois"), 
+                    yyyy: int = commands.parameter(description="Année"), 
+                    hh: int = commands.parameter(description="Heure"), 
+                    mm: int = commands.parameter(description="Minutes")):
+        channel_id = '1159147161255161906'
+
+        # Convert the provided date and time arguments into a datetime object
+        start_date = datetime(yyyy, MM, dd, hh, mm, 0)
+        end_date = datetime.utcnow()
+
+        channel = bot.get_channel(int(channel_id))
+
+        # Fetch messages within the specified timeframe
+        messages = []
+        async for message in channel.history(limit=None, after=start_date, before=end_date):
+            messages.append({
+                'timestamp': message.created_at.strftime('%d/%m %H:%M'),
+                'user': message.author.name,
+                'message': message.content
+            })
+
+        # Write messages to a CSV file
+        csv_file_path = f'messages-list/messages_{start_date.strftime("%Y%m%d%H%M")}_{end_date.strftime("%Y%m%d%H%M")}.csv'
+        with open(csv_file_path, 'w', newline='', encoding='utf-8') as csv_file:
+            fieldnames = ['timestamp', 'user', 'message']
+            writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+            
+            # Write the header
+            writer.writeheader()
+
+            # Write messages
+            writer.writerows(messages)
+
+        print(f'Messages written to {csv_file_path}')
+
+
+    def parse_csv_file(file_path):
+        user_counts = {}
+        
+        with open(file_path, 'r', newline='', encoding='utf-8') as csv_file:
+            reader = csv.DictReader(csv_file, delimiter=';')
+            
+            for row in reader:
+                user = row['User']
+                counts = {
+                    'couscous': int(row['couscous']),
+                    'kayak': int(row['kayak']),
+                    'ck': int(row['ck']),
+                    'Total': int(row['Total'])
+                }
+                
+                user_counts[user] = counts
+
+        return user_counts
+
+    @commands.command(name="recap", help="Récap des scores")
+    async def recap(self, ctx):
+        parsed_results = parse_csv_file("./results/global.csv")
+        parsed_sorted = dict(sorted(parsed_results.items(), key=lambda item: item[1]['Total'], reverse=True))
+        
+        user_l = "User"
+        filler = "-"
+        
+        message_content = "# Scores\n"
+        message_content += "```"+user_l.ljust(16, " ")+"| Couscous | Kayak | Combo | Total\n"
+        message_content+= filler.ljust(50, "-") + "\n"
+        
+        for user in parsed_sorted:
+            message_content += user.ljust(16, " ") +"| " + str(parsed_sorted[user]["couscous"]).ljust(8, " ")+" | " +str(parsed_sorted[user]["kayak"]).ljust(5, " ") +" | " +str(parsed_sorted[user]["ck"]).ljust(5, " ") +" | " +str(parsed_sorted[user]["Total"]) +"\n"
+        
+        message_content += "```"
+        await ctx.send(message_content)
+    
+
+###########################################################################################################################################
+    
+    
+class Custom(commands.Cog, name="Custom Commands"):
+    def __init__(self, bot):
+        self.bot = bot
+        
+    @commands.command(name="repond", help="Répond wesh !")
+    async def repond(
+        self,
+        ctx, 
+        user : str = commands.parameter(description="Mention de l'utilisateur")):
+        for i in range(5):
+            await ctx.send("wesh " + user + " réponds !!!!!!")
+
+
+###########################################################################################################################################
+
+
+class Ping(commands.Cog, name="Ping Commands"):
+    def __init__(self, bot):
+        self.bot = bot
+        
+    @commands.command(name="blip", help="Je suis un robot")
+    async def blip(self, ctx):
+        await ctx.send("```blip bloup je suis un robot```")
+        
+
+###########################################################################################################################################
+
 @bot.event
 async def on_ready():
     print(f'We have logged in as {bot.user.name}')
-    
-
-@bot.command(name="count", help="Laissez Fanny faire siouplé")
-async def count(ctx, 
-                dd: int = commands.parameter(description="Jour"), 
-                MM: int = commands.parameter(description="Mois"), 
-                yyyy: int = commands.parameter(description="Année"), 
-                hh: int = commands.parameter(description="Heure"), 
-                mm: int = commands.parameter(description="Minutes")):
-    channel_id = '1159147161255161906'
-
-    # Convert the provided date and time arguments into a datetime object
-    start_date = datetime(yyyy, MM, dd, hh, mm, 0)
-    end_date = datetime.utcnow()
-
-    channel = bot.get_channel(int(channel_id))
-
-    # Fetch messages within the specified timeframe
-    messages = []
-    async for message in channel.history(limit=None, after=start_date, before=end_date):
-        messages.append({
-            'timestamp': message.created_at.strftime('%d/%m %H:%M'),
-            'user': message.author.name,
-            'message': message.content
-        })
-
-    # Write messages to a CSV file
-    csv_file_path = f'messages-list/messages_{start_date.strftime("%Y%m%d%H%M")}_{end_date.strftime("%Y%m%d%H%M")}.csv'
-    with open(csv_file_path, 'w', newline='', encoding='utf-8') as csv_file:
-        fieldnames = ['timestamp', 'user', 'message']
-        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-        
-        # Write the header
-        writer.writeheader()
-
-        # Write messages
-        writer.writerows(messages)
-
-    print(f'Messages written to {csv_file_path}')
-
-
-def parse_csv_file(file_path):
-    user_counts = {}
-    
-    with open(file_path, 'r', newline='', encoding='utf-8') as csv_file:
-        reader = csv.DictReader(csv_file, delimiter=';')
-        
-        for row in reader:
-            user = row['User']
-            counts = {
-                'couscous': int(row['couscous']),
-                'kayak': int(row['kayak']),
-                'ck': int(row['ck']),
-                'Total': int(row['Total'])
-            }
-            
-            user_counts[user] = counts
-
-    return user_counts
-
-
-@bot.command(name="recap", help="Récap des scores")
-async def recap(ctx):
-    parsed_results = parse_csv_file("./results/global.csv")
-    parsed_sorted = dict(sorted(parsed_results.items(), key=lambda item: item[1]['Total'], reverse=True))
-    
-    user_l = "User"
-    filler = "-"
-    
-    message_content = "# Scores\n"
-    message_content += "```"+user_l.ljust(16, " ")+"| Couscous | Kayak | Combo | Total\n"
-    message_content+= filler.ljust(50, "-") + "\n"
-    
-    for user in parsed_sorted:
-        message_content += user.ljust(16, " ") +"| " + str(parsed_sorted[user]["couscous"]).ljust(8, " ")+" | " +str(parsed_sorted[user]["kayak"]).ljust(5, " ") +" | " +str(parsed_sorted[user]["ck"]).ljust(5, " ") +" | " +str(parsed_sorted[user]["Total"]) +"\n"
-    
-    message_content += "```"
-    await ctx.send(message_content)
-    
-
-@bot.command(name="repond", help="Répond wesh !")
-async def repond(ctx, user : str = commands.parameter(description="Mention de l'utilisateur")):
-    for i in range(5):
-        await ctx.send("wesh " + user + " réponds !!!!!!")
-
-
-
-@bot.command(name="blip", help="Je suis un robot")
-async def blip(ctx):
-    await ctx.send("```blip bloup je suis un robot```")
-    
+    await bot.add_cog(Couscous(bot))
+    await bot.add_cog(Custom(bot))
+    await bot.add_cog(Ping(bot))
 
 
 # Run the bot with the token
